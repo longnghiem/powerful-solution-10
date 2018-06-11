87bca6740021ea03a4d3ce6bdaf1ab5e788dd2d5
Long Nghiem <longsuomi@gmail.com>
2018-06-11 10:45:15 +0300

    using state for tabs

--- src/components/Development_tab.js
+++ src/components/Development_tab.js
@@ -1,37 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const development_tab = () => {
+class Development_tab extends Component {
 
-
-  const toggleTabHandler = (content_show,content_hidden,tab_active,tab_inactive) => {
-    document.querySelector(content_show).classList.remove('hidden')
-    document.querySelector(content_hidden).classList.add('hidden')
-    document.querySelector(tab_active).classList.remove('inactive')
-    document.querySelector(tab_inactive).classList.add('inactive')
+  state = {
+    label_design_class: 'h3-design',
+    label_production_class: 'h3-production inactive',
+    content_design_class: 'design',
+    content_production_class: 'production hidden',
+    isInactive: false,
   }
 
-  return (
-    <section>
-      <h3 onClick={()=>toggleTabHandler('.design','.production','.h3-design','.h3-production')} className='h3-design'>DESIGN</h3>
-      <h3 onClick={()=>toggleTabHandler('.production','.design','.h3-production','.h3-design')}  className='h3-production inactive'>PRODUCTION</h3>
-      <div className="design">
-        <h4>The Design</h4>
-        <p>eanim summis cillum labore illum sunt veniam veniam multos quae aliqua nulla minim quis anim quis elit quis anim quis elit quis anim quis elit quis anim quis elit quis anim quis elitaute export fugiat summis dolor duis irure irure quid esse magna esse export fore</p>
-        <p>elit magna anim summis cillum labore illum sunt veniam veniam multos quae aliqua
-          nulla miniduis irure irure quid essenim quis anim quis elnim quis anim quis el
-          magna esse export fore</p>
-        <a href='#' className='button'>READ MORE</a>
-      </div>
-      <div className="production hidden">
-        <h4>The Production</h4>
-        <p>veniam ua nulla minim quis anim quis elit quis anim quis elit quis anim quis elit quis anim quis elit quis anim quis elitaute export fugiat summis dolor duis irure irure quid esse magna esse export fore</p>
-        <p>eanim summis aliquanim quis anim quis elnim quis anim quis el
-          nulla minim quis anim quis elit quis anim quis elit quis anim quis elit quis anim quis elit quis anim quis elitaute export fugiat summis dolor duis irure irure quid esse
-          magna esse export fore</p>
-        <a href='#' className='button'>READ MORE</a>
-      </div>
-    </section>
-  )
+  toggleTabHandler = (bool) => {
+    if (bool) {
+      this.setState({
+        label_production_class: this.state.label_production_class + ' inactive',
+        label_design_class: this.state.label_design_class.replace(' inactive',''),
+        content_production_class: this.state.content_production_class + ' hidden',
+        content_design_class: this.state.content_design_class.replace(' hidden',''),
+      })
+    } else {
+      this.setState({
+        label_design_class: this.state.label_design_class + ' inactive',
+        label_production_class: this.state.label_production_class.replace(' inactive',''),
+        content_design_class: this.state.content_design_class + ' hidden',
+        content_production_class: this.state.content_production_class.replace(' hidden',''),
+      })
+    }
+  }
+
+  render(){
+    return (
+      <section>
+        <h3 onClick={()=>this.toggleTabHandler(true)} className={this.state.label_design_class}>DESIGN</h3>
+        <h3 onClick={()=>this.toggleTabHandler(false)}  className={this.state.label_production_class}>PRODUCTION</h3>
+        <div className={this.state.content_design_class}>
+          <h4>The Design</h4>
+          <p>eanim summis cillum labore illum sunt veniam veniam multos quae aliqua nulla minim quis anim quis elit quis anim quis elit quis anim quis elit quis anim quis elit quis anim quis elitaute export fugiat summis dolor duis irure irure quid esse magna esse export fore</p>
+          <p>elit magna anim summis cillum labore illum sunt veniam veniam multos quae aliqua
+            nulla miniduis irure irure quid essenim quis anim quis elnim quis anim quis el
+            magna esse export fore</p>
+          <a href='#' className='button'>READ MORE</a>
+        </div>
+        <div className={this.state.content_production_class}>
+          <h4>The Production</h4>
+          <p>veniam ua nulla minim quis anim quis elit quis anim quis elit quis anim quis elit quis anim quis elit quis anim quis elitaute export fugiat summis dolor duis irure irure quid esse magna esse export fore</p>
+          <p>eanim summis aliquanim quis anim quis elnim quis anim quis el
+            nulla minim quis anim quis elit quis anim quis elit quis anim quis elit quis anim quis elit quis anim quis elitaute export fugiat summis dolor duis irure irure quid esse
+            magna esse export fore</p>
+          <a href='#' className='button'>READ MORE</a>
+        </div>
+      </section>
+    )
+  }
+
 }
 
-export default development_tab;
+export default Development_tab;
